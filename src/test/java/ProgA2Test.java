bc6a12766912e8b9538652ae05042523ceed735a
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-05 17:48:48 +0900

    Update ProgA2Test.java

--- src/test/java/ProgA2Test.java
+++ src/test/java/ProgA2Test.java
@@ -1,7 +1,9 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import java.io.*;
-
+/**
+ * @version (20220605)
+ **/
 public class ProgA2Test {
 
     @Test
@@ -18,12 +20,13 @@ public class ProgA2Test {
         // in.inputln("2"); // 標準入力をテストする場合
         // Hello.main(new String[]{"1", "2", "3"}); // 実行時引数をテストする場合
         ProgA2.main(null);
-
-        // assertion
-        String[] prints = bos.toString().split("\n");
-        assertEquals("最終HPは10でした。\n", prints[prints.length - 1]);
-
         // undo the binding in System
         System.setOut(originalOut);
+
+        // assertion
+        String[] prints = bos.toString().split("\r\n|\n", -1);
+        assertEquals("最終HPは10でした。", prints[prints.length - 2],
+            "main()メソッドのprint文の最終行が 最終HPは10でした。 となっていません!");
     }
+
 }
